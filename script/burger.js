8aa3413f7d6e1555ab8a54d5a55f8ff8e2955499
Daniel Khabibullin <daniel.khabibullin@gmail.com>
2023-12-14 09:07:02 +0500

    Add rework main, burger

--- script/burger.js
+++ script/burger.js
@@ -1,12 +1,105 @@
 "use strict";
+
 document.addEventListener("DOMContentLoaded", function() {
+  const scrollController = {
+    scrollPosition: 0,
+    isScrollDisabled: false,
+    disabledScroll() {
+      scrollController.scrollPosition = window.scrollY;
+      document.body.style.cssText = `
+        overflow: hidden;
+        position: fixed;
+        top: -${scrollController.scrollPosition}px;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        padding-right: ${window.innerWidth - document.body.offsetWidth}px
+      `;
+      document.documentElement.style.scrollBehavior = 'unset';
+      this.isScrollDisabled = true;
+    },
+    enabledScroll() {
+      document.body.style.cssText = '';
+      window.scroll({top: scrollController.scrollPosition})
+      document.documentElement.style.scrollBehavior = '';
+      this.isScrollDisabled = false;
+    },
+  }
+  let modalOpened = false;
+  
+  const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
+    const buttonElems = document.querySelectorAll(btnOpen);
+    const modalElem = document.querySelector(modal);
+  
+    modalElem.style.cssText = `
+      display: flex;
+      visibility: hidden;
+      opacity: 0;
+      transition: opacity ${time}ms ease-in-out;
+    `;
+  
+    const closeModal = event => {
+      const target = event.target;
+  
+      if (
+        target === modalElem ||
+        (btnClose && target.closest(btnClose)) ||
+        event.code === 'Escape'
+        ) {
+        
+        modalElem.style.opacity = 0;
+  
+        setTimeout(() => {
+          modalElem.style.visibility = 'hidden';
+          if (modalOpened) {
+            modalOpened = false;
+            scrollController.enabledScroll();
+          }
+        }, time);
+  
+        window.removeEventListener('keydown', closeModal);
+      }
+    }
+  
+    const openModal = () => {
+      if (!modalOpened) {
+        modalOpened = true;
+        modalElem.style.visibility = 'visible';
+        modalElem.style.opacity = 1;
+        window.addEventListener('keydown', closeModal);
+        scrollController.disabledScroll();
+      }
+    };
+  
+    buttonElems.forEach(btn => {
+      btn.addEventListener('click', openModal);
+    });
+  
+  
+    modalElem.addEventListener('click', closeModal);
+  };
+  modalController({
+    modal: '.modal1',
+    btnOpen: '.btn_modal1',
+    btnClose: '.modal__close',
+  });
+  // modalController({
+  //   modal: '.modal2',
+  //   btnOpen: '.btn_modal2',
+  //   btnClose: '.modal__close'
+  // });
+
   //burger
   const burger = document.querySelector('.burger');
-  const navigation = document.querySelectorAll('.navigation')[0];
+  const navigation = document.querySelector('.navigation');
   // const headerWhiteBefore = document.querySelector('.header_opaque_before');
   if (burger) {
     burger.addEventListener('click', () => {
-      document.body.classList.toggle('_lock');
+      if (!scrollController.isScrollDisabled) {
+        scrollController.disabledScroll();
+      } else {
+        scrollController.enabledScroll();
+      }
       burger.classList.toggle('burger_active');
       navigation.classList.toggle('navigation_active');
       // headerWhiteBefore.classList.toggle('visible');
@@ -33,8 +126,10 @@ document.addEventListener("DOMContentLoaded", function() {
       navLink.classList.add('navigation__link_active');
       if (burger.classList.contains('burger_active')) {
         burger.classList.remove('burger_active');
-        document.body.classList.remove('_lock');
         navigation.classList.remove('navigation_active');
+      }
+      if (scrollController.isScrollDisabled) {
+        scrollController.enabledScroll();
       }
 
       window.scrollTo({
--- script/modal.js
+++ script/modal.js
@@ -1,84 +1,84 @@
-const scrollController = {
-  scrollPosition: 0,
-  disabledScroll() {
-    scrollController.scrollPosition = window.scrollY;
-    document.body.style.cssText = `
-      overflow: hidden;
-      position: fixed;
-      top: -${scrollController.scrollPosition}px;
-      left: 0;
-      height: 100vh;
-      width: 100vw;
-      padding-right: ${window.innerWidth - document.body.offsetWidth}px
-    `;
-    document.documentElement.style.scrollBehavior = 'unset';
-  },
-  enabledScroll() {
-    document.body.style.cssText = '';
-    window.scroll({top: scrollController.scrollPosition})
-    document.documentElement.style.scrollBehavior = '';
-  },
-}
-let modalOpened = false;
+// const scrollController = {
+//   scrollPosition: 0,
+//   disabledScroll() {
+//     scrollController.scrollPosition = window.scrollY;
+//     document.body.style.cssText = `
+//       overflow: hidden;
+//       position: fixed;
+//       top: -${scrollController.scrollPosition}px;
+//       left: 0;
+//       height: 100vh;
+//       width: 100vw;
+//       padding-right: ${window.innerWidth - document.body.offsetWidth}px
+//     `;
+//     document.documentElement.style.scrollBehavior = 'unset';
+//   },
+//   enabledScroll() {
+//     document.body.style.cssText = '';
+//     window.scroll({top: scrollController.scrollPosition})
+//     document.documentElement.style.scrollBehavior = '';
+//   },
+// }
+// let modalOpened = false;
 
-const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
-  const buttonElems = document.querySelectorAll(btnOpen);
-  const modalElem = document.querySelector(modal);
+// const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
+//   const buttonElems = document.querySelectorAll(btnOpen);
+//   const modalElem = document.querySelector(modal);
 
-  modalElem.style.cssText = `
-    display: flex;
-    visibility: hidden;
-    opacity: 0;
-    transition: opacity ${time}ms ease-in-out;
-  `;
+//   modalElem.style.cssText = `
+//     display: flex;
+//     visibility: hidden;
+//     opacity: 0;
+//     transition: opacity ${time}ms ease-in-out;
+//   `;
 
-  const closeModal = event => {
-    const target = event.target;
+//   const closeModal = event => {
+//     const target = event.target;
 
-    if (
-      target === modalElem ||
-      (btnClose && target.closest(btnClose)) ||
-      event.code === 'Escape'
-      ) {
+//     if (
+//       target === modalElem ||
+//       (btnClose && target.closest(btnClose)) ||
+//       event.code === 'Escape'
+//       ) {
       
-      modalElem.style.opacity = 0;
+//       modalElem.style.opacity = 0;
 
-      setTimeout(() => {
-        modalElem.style.visibility = 'hidden';
-        if (modalOpened) {
-          modalOpened = false;
-          scrollController.enabledScroll();
-        }
-      }, time);
+//       setTimeout(() => {
+//         modalElem.style.visibility = 'hidden';
+//         if (modalOpened) {
+//           modalOpened = false;
+//           scrollController.enabledScroll();
+//         }
+//       }, time);
 
-      window.removeEventListener('keydown', closeModal);
-    }
-  }
+//       window.removeEventListener('keydown', closeModal);
+//     }
+//   }
 
-  const openModal = () => {
-    if (!modalOpened) {
-      modalOpened = true;
-      modalElem.style.visibility = 'visible';
-      modalElem.style.opacity = 1;
-      window.addEventListener('keydown', closeModal);
-      scrollController.disabledScroll();
-    }
-  };
+//   const openModal = () => {
+//     if (!modalOpened) {
+//       modalOpened = true;
+//       modalElem.style.visibility = 'visible';
+//       modalElem.style.opacity = 1;
+//       window.addEventListener('keydown', closeModal);
+//       scrollController.disabledScroll();
+//     }
+//   };
 
-  buttonElems.forEach(btn => {
-    btn.addEventListener('click', openModal);
-  });
+//   buttonElems.forEach(btn => {
+//     btn.addEventListener('click', openModal);
+//   });
 
 
-  modalElem.addEventListener('click', closeModal);
-};
-modalController({
-  modal: '.modal1',
-  btnOpen: '.btn_modal1',
-  btnClose: '.modal__close',
-});
-modalController({
-  modal: '.modal2',
-  btnOpen: '.btn_modal2',
-  btnClose: '.modal__close'
-});
+//   modalElem.addEventListener('click', closeModal);
+// };
+// modalController({
+//   modal: '.modal1',
+//   btnOpen: '.btn_modal1',
+//   btnClose: '.modal__close',
+// });
+// modalController({
+//   modal: '.modal2',
+//   btnOpen: '.btn_modal2',
+//   btnClose: '.modal__close'
+// });
